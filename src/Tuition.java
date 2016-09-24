f1fbfcbc9c4fd9972f16b3471785c5974333b602
skothare <skothare@128.4.99.4>
2016-09-24 19:39:04 -0400

    First Commit for Quiz 2 Problem 1

---
+++ src/Tuition.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tuition {
+
+	public static void main(String[] args) {
+		// Calculates the total tuition for the 4-year undergraduate program
+		Scanner input = new Scanner(System.in);
+		System.out.println("\n Enter your first year's cost of tuition (in $)");
+		double Tuition = input.nextInt();	
+		
+		double TotalTuition = Tuition;
+		int count = 1;
+		for(int i=1; i<4; i++){
+			count++;
+			System.out.println("\n Enter your approximate percent increase in tution for year " + count);
+			double PercentIncrease = input.nextInt();
+			Tuition += Tuition * (PercentIncrease/100.0);
+			System.out.printf("\n Your tuition for year " + count + " is $ %4.2f",Tuition);
+			TotalTuition += Tuition;
+		}
+		System.out.printf("\n Your total cost of tuition for the " + count + " years is $ %4.2f", TotalTuition);
+	}
+
+}
